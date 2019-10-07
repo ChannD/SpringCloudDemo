08cdc7744ed59870f3d69e1b2e78e44ee8db66ad
JiangLong <jl@jiangdalong.com>
2019-10-07 17:15:05 +0800

    Initial commit

---
+++ microservicecloud-provider-dept-8003/src/main/java/com/chann/springcloud/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.chann.springcloud.service;
+
+import com.chann.springcloud.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+}
